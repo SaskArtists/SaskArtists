b98a5369e767515663af2b03117f3acbc64f59b0
gemohandy <bennetteidsness@gmail.com>
2016-07-19 18:29:28 -0600

    #16 work
    - Added more text
    - Made Tsubo work

--- mtagseth/javascript/tsuboSwitch.js
+++ mtagseth/javascript/tsuboSwitch.js
@@ -1,11 +1,13 @@
-var unfolded = false;
+var unfolded = true;
 function toggleTsubo(){
   tsubo = document.getElementById('tsubo');
   if(unfolded){
-    tsubo.src = 'images/tsuboUnfold.jpg'
-    console.log()
+    tsubo.src = 'images/tsuboUnfold.jpg';
+    tsubo.style.width = '700px';
   }
   else{
     tsubo.src = 'images/TSUBO.jpg'
+    tsubo.style.width = '300px';
   }
+  unfolded = !unfolded;
 }
