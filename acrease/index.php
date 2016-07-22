01fea5cff2e6509b3d32dfbbd8691e3325650436
Brett Mayson <brett@bmandesigns.com>
2016-07-22 03:17:11 -0600

    Fixes

---
+++ acrease/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Alex Crease
+        </title>
+        <meta name="city" content="Regina">
+    </head>
+    <body>
+        <?php include("title.html"); ?>
+        <br/>
+        <?php include("homepage.html"); ?>
+        <?php include("galpage.html"); ?>
+        <?php include("conpage.html"); ?>
+    </body>
+</html>
