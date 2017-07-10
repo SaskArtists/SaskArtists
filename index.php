653cc347d53aad8c764fc10f0e019d6982f3ab7d
Ryan Carignan <ryancarignan16@gmail.com>
2017-07-10 11:17:34 -0600

    fix #296

--- index.php
+++ index.php
@@ -348,9 +348,9 @@ if ($db->connect_errno) {
     <footer class="text-center">
 	<hr>
 	<p>Visit us on <a href='https://github.com/SaskArtists/SaskArtists/'>GitHub</a></p>
-	<p> <a href="http://www.metric-hosting.ca/">Sponsor</a> <br></p>
-	<p><a href="disclaimer.html">Disclaimer</a></p>
-	<p><a href="https://github.com/SaskArtists/SaskArtists/compare/<?php echo file_get_contents("../git.last"); ?>...<?php echo shell_exec("git log --pretty=format:'%H' -n 1"); ?>">Revised: <?php echo shell_exec("git log -1 --format=%cd"); ?> - <?php echo shell_exec("git log --pretty=format:'%h' -n 1"); ?></a></p>
+	<p> <a href="http://www.metric-hosting.ca/" target="blank">Sponsor</a> <br></p>
+	<p><a href="disclaimer.html" target="blank">Disclaimer</a></p>
+	<p><a href="https://github.com/SaskArtists/SaskArtists/compare/ target="blank" <?php echo file_get_contents("../git.last"); ?>...<?php echo shell_exec("git log --pretty=format:'%H' -n 1"); ?>">Revised: <?php echo shell_exec("git log -1 --format=%cd"); ?> - <?php echo shell_exec("git log --pretty=format:'%h' -n 1"); ?></a></p>
 </footer>
 </div>
 
