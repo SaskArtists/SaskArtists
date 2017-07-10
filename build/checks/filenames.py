075f6eae437e74ef148b5713735514daaecaa65a
Brett Mayson <brett@bmandesigns.com>
2017-07-09 21:47:09 -0600

    descriptive name to warning, invalid characters

--- build/checks/filenames.py
+++ build/checks/filenames.py
@@ -1,6 +1,8 @@
 import os, sys
 
 invalid_names = ["image","art","picture","pic","img"]
+invalid_characters = ["(",")","'","\"","%","&","^","=","+","?",","]
+invalid_end_char = ["_","."]
 
 scandir = ""
 try:
@@ -16,10 +18,18 @@ for root, dirs, files in os.walk(scandir):
         if " " in file:
             ret = 1
             print "Space in filename: ",os.path.join(root,file)
+        for i in invalid_characters:
+            if i in file:
+                print "Invalid Character "+i+":",os.path.join(root,file)
+                ret = 1
+        for i in invalid_end_char:
+            if file[-1] == i:
+                print "Invalid Ending "+i+":",os.path.join(root,file)
+                ret = 1
         for i in invalid_names:
             if len(file.replace(i,"")) < 8 and "." in file:
                 print "Undescriptive name:",os.path.join(root,file)
-                ret = 1
+                #ret = 1
                 break
 
 sys.exit(ret)
