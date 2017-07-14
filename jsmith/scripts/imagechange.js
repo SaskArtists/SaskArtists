1e827a74b0ccfd26abc5634fcb3dcaf2357776cb
Austin Kearns <austinkearnssk@gmail.com>
2017-07-13 20:02:01 -0600

    Fixed issues

--- jsmith/scripts/imagechange.js
+++ jsmith/scripts/imagechange.js
@@ -2,6 +2,6 @@ $(function () {
   $("#frontpageimg").mouseenter(function(){
     $(this).attr("src","images/horse.jpg");
   }).mouseleave(function(){
-    $(this).attr("src","images/dog.jpg");
+    $(this).attr("src","images/doggrass.jpg");
   });
 });
