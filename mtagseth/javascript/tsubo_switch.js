15099268d22b82ea44d5f327296e0904153f9b7c
Bennett <bennetteidsness@gmail.com>
2018-07-05 18:35:32 -0600

    Martin Tagseth Compliance Update

--- mtagseth/javascript/tsubo_switch.js
+++ mtagseth/javascript/tsubo_switch.js
@@ -2,11 +2,11 @@ var unfolded = true;
 function toggleTsubo(){
   tsubo = document.getElementById('tsubo');
   if(unfolded){
-    tsubo.src = 'images/tsuboUnfold.jpg';
+    tsubo.src = 'images/tsubo_unfold.jpg';
     tsubo.style.width = '700px';
   }
   else{
-    tsubo.src = 'images/TSUBO.jpg'
+    tsubo.src = 'images/tsubo_fold.jpg'
     tsubo.style.width = '300px';
   }
   unfolded = !unfolded;
