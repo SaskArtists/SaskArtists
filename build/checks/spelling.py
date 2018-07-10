dab1ee11b6378db560f8c242418f8d84f5aada6c
gemohandy <bennetteidsness@gmail.com>
2018-07-09 20:52:17 -0600

    Reset spelling.py to functional state. I hope.

--- build/checks/spelling.py
+++ build/checks/spelling.py
@@ -6,21 +6,18 @@ ROOT = "./www/artists"
 
 ret = 0
 
-#with open("./build/spelling_allowed.txt") as fobj:
-#    allowed = fobj.read().split("\n")
+with open("./build/spelling_allowed.txt") as fobj:
+    allowed = fobj.read().split("\n")
 
 most = {}
 
 for artist in filter(lambda x: os.path.isdir(os.path.join(ROOT, x)), os.listdir(ROOT)):
-    print(artist)
     for dir in os.walk(os.path.join(ROOT, artist)):
         for file in dir[2]:
             if not file.endswith(".html"): continue
             path = os.path.join(dir[0], file)
-            print(path)
             errors = subprocess.getoutput("cat {} | aspell -a -H".format(path)).split("\n")[1:]
             for line in errors:
-                print(line)
                 if line.strip() in ["*", ""]: continue
                 word = line.strip().split(" ")[1]
                 if word in allowed: continue
