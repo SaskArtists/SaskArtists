130ed64e9e60967a5cc402d42eea00349296bb9c
Kaitlin <kzastrow01@gmail.com>
2018-07-13 11:16:54 -0600

    Added all artists and search works.

--- provision.sh
+++ provision.sh
@@ -30,16 +30,6 @@ gem install listen
 echo -e "\n--- Updating packages list ---\n"
 apt-get -qq update
 
-
-echo -e "\n--- Installing PHP-specific packages ---\n"
-apt-get -y install php apache2 libapache2-mod-php php-curl php-gd php-mysql php-gettext >> /vagrant/vm_build.log 2>&1
-
-echo -e "\n--- Installing composer packages ---\n"
-cd /vagrant
-composer install
-cd /vagrant/www
-composer install
-
 echo -e "\n--- Enabling mod-rewrite ---\n"
 a2enmod rewrite >> /vagrant/vm_build.log 2>&1
 
@@ -50,17 +40,9 @@ echo -e "\n--- Setting document root to vagrant directory ---\n"
 rm -rf /var/www/html
 ln -fs /vagrant/www/ /var/www/html
 
-echo -e "\n--- Importing fake data into MySQL ---\n"
-sh /vagrant/tools/mysql_import.sh
-
 echo -e "\n--- We definitly need to see the PHP errors, turning them on ---\n"
 sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.0/apache2/php.ini
 sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.0/apache2/php.ini
-
-echo "----- Copying Apache2 Configurations"
-cp /vagrant/vm_provision/dir.conf /etc/apache2/mods-enabled/dir.conf
-cp /vagrant/vm_provision/project.conf /etc/apache2/sites-available/project.conf
-a2ensite project.conf
 
 echo -e "\n--- Restarting Apache ---\n"
 service apache2 restart >> /vagrant/vm_build.log 2>&1
--- www/js/index.js
+++ www/js/index.js
@@ -1,22 +1,42 @@
 var artistsApp = new Vue({
   el: "#artists-app",
   data: {
-    name: [],
-    location: [],
-    changelog: [],
+    artists: [],
+    search: ''
   },
 
   created:function() {
     this.loadArtists();
   },
-
+  computed: {
+    display: function() {
+      this.search;
+      var display = [];
+      this.artists.forEach(function(artist){
+          display.push(artist);
+      });
+      if(this.search == ""){
+        return display;
+      }
+      else{
+        return display.filter(searchForStart, this.search);
+      }
+    }
+  },
   methods: {
     loadArtists: function() {
+      var vm = this;
       var urlToGet = window.location.protocol + "//" + window.location.host + "/artists.json";
       axios.get(urlToGet)
         .then(function(response){
-          console.log(response["data"]);
+          vm.artists = response["data"];
+        }).catch(function(response){
+          console.log(response);
         });
     }
   }
-})
+});
+
+function searchForStart(test){
+  return test.name.toLowerCase().includes(this);
+}
