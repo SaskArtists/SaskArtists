8888d3e40d7bbfbb0df756dd74bcc0c1ad4450f2
Brett Mayson <brett@bmandesigns.com>
2018-08-19 10:21:00 -0600

    Update deploy

--- build/scripts/deploy.py
+++ build/scripts/deploy.py
@@ -6,25 +6,37 @@ import sys
 
 #Get Environment Variables
 
-COMMIT_CURR = subprocess.getoutput("git rev-parse HEAD")
-print("COMMIT_CURR {}".format(COMMIT_CURR))
+def run(cmd):
+    subprocess.check_call(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 
-if os.path.exists("./deploy-info/commit"):
-    with open("./deploy-info/commit") as f:
+DIR = "/tmp/project/workspace/build-output/deploy-info/"
+COMMIT_FILE = DIR + "commit"
+
+DEST = "saskarti@saskartists.ca:/home/saskarti/www"
+
+COMMIT_CURR = subprocess.getoutput("git rev-parse HEAD")
+print(f"COMMIT_CURR {COMMIT_CURR}")
+
+if os.path.exists(COMMIT_FILE):
+    with open(COMMIT_FILE) as f:
         COMMIT_PREV = f.read().strip()
-        print("COMMIT_PREV {}".format(COMMIT_PREV))
+        print(f"COMMIT_PREV {COMMIT_PREV}")
 else:
-    os.system("scp -oStrictHostKeyChecking=no -r www/* saskarti@saskartists.ca:/home/saskarti/www/")
-    os.system("scp -oStrictHostKeyChecking=no -r www/.htaccess saskarti@saskartists.ca:/home/saskarti/www/.htaccess")
-    with open("./deploy-info/commit", "w") as f:
+    print("no previous commit, sending all")
+    run(["scp", "-oStrictHostKeyChecking=no", "-r", "www/*", f"{DEST}"])
+    run(["scp", "-oStrictHostKeyChecking=no", "-r", "www/.htaccess", f"{DEST}/.htaccess"])
+    with open(COMMIT_FILE, "w") as f:
         f.write(COMMIT_CURR)
     sys.exit(0)
 
 #Files that have been modified since the last CircleCI build
-CHANGED_FILES = subprocess.getoutput("git diff --name-only {} {}".format(COMMIT_PREV, COMMIT_CURR))
+CHANGED_FILES = subprocess.getoutput(f"git diff --name-only {COMMIT_PREV} {COMMIT_CURR}")
 CHANGED_FILES = CHANGED_FILES.split("\n")
 
 print(CHANGED_FILES)
 
-with open("./deploy-info/commit", "w") as f:
+if not os.path.exists(DIR):
+    os.makedirs(DIR)
+
+with open(COMMIT_FILE, "w") as f:
     f.write(COMMIT_CURR)
