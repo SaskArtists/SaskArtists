23eec79f2f87ae248d3b30fc691ba4841173ff56
Brett <brett@bmandesigns.com>
2019-01-30 17:37:27 -0600

    Update googleanalytics.py
    just some spacing, and I removed the comments

--- build/scripts/googleanalytics.py
+++ build/scripts/googleanalytics.py
@@ -1,19 +1,17 @@
 import bs4, os
 
-# load the file
 for root, dirs, filename in os.walk('www/'):
-  if filename.endswith('.html'):
-    file = os.path.join(root, filename)
-    with open(file) as inf:
-      txt = inf.read()
-      soup = bs4.BeautifulSoup(txt)
+    if filename.endswith('.html'):
+        file = os.path.join(root, filename)
+        with open(file) as inf:
+            txt = inf.read()
+            soup = bs4.BeautifulSoup(txt)
     
-    # create new link"link", rel
-    new_link = soup.new_tag("script", async="true", src="https://www.googletagmanager.com/gtag/js?id=UA-129512949-1")
-    # insert it into the document
-    soup.head.append(new_link)
-    gtag = soup.new_tag("script")
-    gtag.string = "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-129512949-1');"
-    soup.head.append(gtag)
-    with open(file, "w") as outf:
-      outf.write(str(soup))
+        gtag = soup.new_tag("script", async="true", src="https://www.googletagmanager.com/gtag/js?id=UA-129512949-1")
+        gtag.head.append(gtag)
+        gtag = soup.new_tag("script")
+        gtag.string = "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-129512949-1');"
+        soup.head.append(gtag)
+    
+        with open(file, "w") as outf:
+          outf.write(str(soup))
