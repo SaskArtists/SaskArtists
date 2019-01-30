f71669c20ebf6bbea92443598c8a9297132bda83
Brett <brett@bmandesigns.com>
2019-01-30 17:46:49 -0600

    python 3
    the example we used was for python 2 👍

--- build/scripts/googleanalytics.py
+++ build/scripts/googleanalytics.py
@@ -4,9 +4,9 @@ for root, dirs, filenames in os.walk('www/'):
     for filename in filenames:
         if filename.split('.')[-1] in ['html', 'htm']:
             file = os.path.join(root, filename)
-            with open(file) as inf:
+            with open(file, encoding="utf-8") as inf:
                 txt = inf.read()
-                soup = bs4.BeautifulSoup(txt)
+                soup = bs4.BeautifulSoup(txt, "html.parser")
     
             gtag = soup.new_tag("script", src="https://www.googletagmanager.com/gtag/js?id=UA-129512949-1")
             gtag.attrs['async'] = None
