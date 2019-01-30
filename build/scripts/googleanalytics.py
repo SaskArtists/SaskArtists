1b0c00c14f3ea872aa3c2c3514a52f0c38a6b212
Brett <brett@bmandesigns.com>
2019-01-30 17:42:31 -0600

    whoops

--- build/scripts/googleanalytics.py
+++ build/scripts/googleanalytics.py
@@ -1,18 +1,19 @@
 import bs4, os
 
-for root, dirs, filename in os.walk('www/'):
-    if filename.split('.')[-1] in ['html', 'htm']:
-        file = os.path.join(root, filename)
-        with open(file) as inf:
-            txt = inf.read()
-            soup = bs4.BeautifulSoup(txt)
+for root, dirs, filenames in os.walk('www/'):
+    for filename in filenames:
+        if filename.split('.')[-1] in ['html', 'htm']:
+            file = os.path.join(root, filename)
+            with open(file) as inf:
+                txt = inf.read()
+                soup = bs4.BeautifulSoup(txt)
     
-        gtag = soup.new_tag("script", src="https://www.googletagmanager.com/gtag/js?id=UA-129512949-1")
-        gtag.attrs['async'] = None
-        gtag.head.append(gtag)
-        gtag = soup.new_tag("script")
-        gtag.string = "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-129512949-1');"
-        soup.head.append(gtag)
+            gtag = soup.new_tag("script", src="https://www.googletagmanager.com/gtag/js?id=UA-129512949-1")
+            gtag.attrs['async'] = None
+            gtag.head.append(gtag)
+            gtag = soup.new_tag("script")
+            gtag.string = "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-129512949-1');"
+            soup.head.append(gtag)
     
-        with open(file, "w") as outf:
-          outf.write(str(soup))
+            with open(file, "w") as outf:
+              outf.write(str(soup))
