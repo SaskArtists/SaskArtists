faec00791f82c1c2cd8a848b498eae882024afc1
trollmad3 <zuckonit@riseup.net>
2019-01-30 18:12:52 -0600

    add autoinjector (#411)
    his script automatically injects the google analytics code into every .html file, without doing it manually.

---
+++ build/scripts/googleanalytics.py
@@ -0,0 +1,22 @@
+import bs4, os
+
+for root, dirs, filenames in os.walk('www/'):
+    for filename in filenames:
+        if filename.split('.')[-1] in ['html', 'htm']:
+            file = os.path.join(root, filename)
+            if "reginabellringers" in file:
+                continue
+            print(file)
+            with open(file, encoding="utf-8") as inf:
+                txt = inf.read()
+                soup = bs4.BeautifulSoup(txt, "html.parser")
+    
+            gtag = soup.new_tag("script", src="https://www.googletagmanager.com/gtag/js?id=UA-50411886-2")
+            gtag.attrs['async'] = None
+            soup.head.append(gtag)
+            gtag = soup.new_tag("script")
+            gtag.string = "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-50411886-2');"
+            soup.head.append(gtag)
+    
+            with open(file, "w") as outf:
+              outf.write(str(soup))
