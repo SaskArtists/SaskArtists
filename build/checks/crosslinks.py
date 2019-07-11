13ae342e7a5998fa18c7d03cc5a777fef075d3e8
gemohandy <bennetteidsness@gmail.com>
2019-07-11 09:13:21 -0600

    Change crosslinks self test to use link This prevents issues with folders that are impropperly named - we have a few of those, after all.

--- build/checks/crosslinks.py
+++ build/checks/crosslinks.py
@@ -7,13 +7,14 @@ artists = []
 with open("./www/artists.json") as artf:
     data = json.loads(artf.read())
     for artist in data:
-        artists.append(artist["name"])
+        artists.append({link: artist["link"], name: artist["name"]})
 
 class Crosslinks(test.Test):
     def check(self, source, text):
         for artist in artists:
-            name = artist.lower().split(" ")
-            if name[0][0] + name[-1] in source: continue
+            link = artist[link].lower()
+            if link in source: continue
+            name = artist["name"].lower.split(" ")
             matches = re.finditer(regex.format(name = artist), text)
             for matchNum, match in enumerate(matches):
                 print(source, f"{artist}")
