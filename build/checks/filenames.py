3b4010fe64c63f81cdf1fb138fbc854220cbb8d2
Brett <brett@bmandesigns.com>
2019-07-12 11:51:53 -0600

    yeet

--- build/checks/filenames.py
+++ build/checks/filenames.py
@@ -20,7 +20,7 @@ for root, dirs, files in os.walk(scandir):
             or ".git" in os.path.join(root, file) \
             or os.path.join(root, file) == "./README.md":
             break
-        if "checks/" in root:
+        if "checks/" in os.path.join(root, file):
             continue
         if " " in file:
             ret += 1
